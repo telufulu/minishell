405ace30c75d82c7d2ca6b416ef60e4c53e941f0
Teresa B. Lufuluabo <telufulu@student.42madrid.com>
2024-10-03 21:16:57 +0200

    deleted executor => I'm going to start again

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:50:41 by telufulu          #+#    #+#             */
-/*   Updated: 2024/10/02 02:03:26 by telufulu         ###   ########.fr       */
+/*   Updated: 2024/10/03 21:15:41 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,54 +32,6 @@ t_cmd	*init_cmd(t_data *d, size_t n)
 	return (res);
 }
 
-void	redirect(t_cmd *c, t_bool i, t_bool pipe, int *pipefd)
-{
-	if (c->infd)
-		dup2(c->infd, STDIN_FILENO);
-	else if (i)
-		dup2(pipefd[0], STDIN_FILENO);
-	if (c->outfd)
-		dup2(c->outfd, STDOUT_FILENO);
-	else if (pipe)
-		dup2(pipefd[1], STDOUT_FILENO);
-}
-
-void	execute_cmd(t_data *d, size_t i, size_t cmd_num, int *pipefd)
-{
-	t_cmd	*c;
-
-	c = init_cmd(d, i);
-	if (c && (!c->path || !c->argv))
-		exit(EXIT_SUCCESS);
-	redirect(c, (i), (i < cmd_num - 1), pipefd);
-	if (execve(c->path, c->argv, c->data->env) == -1)
-		ft_error("execve failed", strerror(errno));
-	ft_free_matrix((void **)c);
-}
-
 void	executor(t_data *d)
 {
-	int		status;
-	size_t	i;
-	size_t	cmd_num;
-	int		pipefd[2];
-	pid_t	pid;
-
-	i = 0;
-	cmd_num = count_cmds(d->tokens);
-	while (i < cmd_num)
-	{
-		if (pipe(pipefd) == -1)
-			ft_error("pipe creation failed", strerror(errno));
-		pid = fork();
-		if (pid < 0)
-			ft_error("fork failed", strerror(errno));
-		else if (!pid)
-		{
-			execute_cmd(d, i, cmd_num, pipefd);
-			exit(EXIT_SUCCESS);
-		}
-		++i;
-	}
-	wait(&status);
 }
