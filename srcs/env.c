07d15f8b023ed46a87976702f4ea05fac7e3471c
Teresa B. Lufuluabo Pastor <teresa@teresalufuluabo.es>
2024-11-15 16:21:09 +0100

    env fixed

--- srcs/env.c
+++ srcs/env.c
@@ -6,7 +6,7 @@
 /*   By: telufulu <telufulu@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 20:10:07 by telufulu          #+#    #+#             */
-/*   Updated: 2024/11/15 15:49:30 by aude-la-         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:20:43 by telufulu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ int	env_except(char *envp, char **res, int *i)
 	{
 		++(*i);
 		aux = ft_itoa(ft_atoi(envp + 6) + 1);
+		free(*res);
 		*res = ft_strjoin("SHLVL=", aux);
 		free(aux);
 		return (1);
